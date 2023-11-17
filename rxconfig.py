c50df12d8a09d79c7e46fde1d455b0ee2bff0751
Ali Fawaz <89460174+eggman920@users.noreply.github.com>
2023-11-17 14:28:22 -0500

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,4 @@
 import reflex as rx
-from pydynamodb import connect
-
-a = connect(aws_access_key_id="AKIAWSY7AU74O3NS7X6W",
-                aws_secret_access_key="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",
-                region_name="us-east-1")
 
 config = rx.Config(
     app_name="reflexprod",
